ed36ae5c2623cbe44622cc62a8f1b05e49d12b8e
Alexander Höreth <ahoereth@gmail.com>
2017-05-07 15:33:00 +0200

    Add week 05 homework outlines.

---
+++ pandas_exercises/pokemon.py
@@ -0,0 +1,36 @@
+"""Scientific Programming in Python Homework 5."""
+
+
+def first_gen(df):
+    """Find the names of all Pokemon from generation 1."""
+    return
+
+
+def highest_hp(df):
+    """Find the name(s) of Pokemon with the highest HP."""
+    return
+
+
+def mean_attack_by_type(df):
+    """Find mean attack power of type 1.
+
+    Return: dataframe with type and attack columns
+    """
+    return
+
+
+def high_defense(df):
+    """Find Name and Defense of Pokemon that have an above average Defense."""
+    return
+
+
+def deduplicated(df):
+    """Find unique pokemons.
+
+    Some Pokemon are in the list multiple times with different names.
+    Find the names of the pokemon but without these duplicates. You
+    can use the # column to tell if they're the same pokemon. Example:
+    Venusaur has # 3 and there is a VenusaurMega Venusaur also
+    with # 3, the result should have just Venusaur
+    """
+    return
