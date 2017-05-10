794c09f9c5e3ac169e5110592ad6226ea4839219
Alexander Höreth <ahoereth@gmail.com>
2017-05-10 14:57:12 +0200

    Solve sheet 05.

--- pandas_exercises/pokemon.py
+++ pandas_exercises/pokemon.py
@@ -1,14 +1,14 @@
 """Scientific Programming in Python Homework 5."""
-
+import pandas as pd
 
 def first_gen(df):
     """Find the names of all Pokemon from generation 1."""
-    return
+    return df[df['Generation'] == 1]['Name']
 
 
 def highest_hp(df):
     """Find the name(s) of Pokemon with the highest HP."""
-    return
+    return df[df['HP'] == df['HP'].max()]['Name']
 
 
 def mean_attack_by_type(df):
@@ -16,12 +16,12 @@ def mean_attack_by_type(df):
 
     Return: dataframe with type and attack columns
     """
-    return
+    return pd.DataFrame(df.groupby('Type 1')['Attack'].mean()).reset_index()
 
 
 def high_defense(df):
     """Find Name and Defense of Pokemon that have an above average Defense."""
-    return
+    return df[df['Defense'] > df['Defense'].mean()][['Name', 'Defense']]
 
 
 def deduplicated(df):
@@ -33,4 +33,16 @@ def deduplicated(df):
     Venusaur has # 3 and there is a VenusaurMega Venusaur also
     with # 3, the result should have just Venusaur
     """
-    return
+    return df.drop_duplicates('#')['Name']
+
+
+def main():
+    """Main function."""
+    try:
+        df = pd.read_csv('Pokemon.csv')
+    except:
+        pass
+
+
+if __name__ == '__main__':
+    main()
